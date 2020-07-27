90443a2257f739f1665f80da35b73fc97f2e1805
Ariel <ariels262626@gmail.com>
2020-07-27 12:38:35 +0300

    sports events first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import ReactMapGL from 'react-map-gl'
+import './App.css';
+import Map from "../components/map/map";
+
+function App() {
+  return (
+    <div className="App">
+        <Map/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/map/map.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import ReactMapGL from 'react-map-gl'
+import './map.css';
+
+function Map() {
+  const [viewport, setViewport] = useState({
+      lat: 33.817690,
+      lang: 33.229659,
+      zoom: 5,
+      width: '50vw',
+      height: '100vh',
+  });
+  return (
+    <div className="map">
+
+      <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/relco/ckd4b9yxx0vp41irwc2mccsa7"
+          onViewportChange={(viewport) => setViewport(viewport)}
+      >
+          markers
+      </ReactMapGL>
+    </div>
+  );
+}
+
+export default Map;
